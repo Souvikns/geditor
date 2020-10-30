ab4ea2ec7a9d25f2c73c33d0080792b38410cde4
Souvik <souvikde.ns@gmail.com>
2020-10-31 00:05:06 +0530

    minor changes

---
+++ src/components/preview.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Preview = props => {
+
+    let { value } = props
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Preview.propTypes = {
+    value: PropTypes.string,
+}
+
+export default Preview
