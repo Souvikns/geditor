bd45b964d01b1931b26a9f28a2340d436ac63050
Souvik <souvikde.ns@gmail.com>
2020-10-31 00:14:27 +0530

    Create editor.jsx

---
+++ src/components/editor.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Editor = props => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+Editor.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+}
+
+export default Editor;
