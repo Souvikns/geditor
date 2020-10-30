0ebc07cd9a4f91540c3f6ffbe6d32200f4ed8124
Souvik <souvikde.ns@gmail.com>
2020-10-31 00:16:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+// importing components 
+import Editor from './components/editor';
+import Preview from './components/preview';
+
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export {
+  Preview
+}
+
+export default Editor
