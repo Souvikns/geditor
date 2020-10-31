6bdb19a59d1f1ad4981df7862fe5165481786110
Souvik <souvikde.ns@gmail.com>
2020-10-31 06:28:50 +0530

    Update preview.jsx

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import marked from 'marked';
 
 const Preview = props => {
 
     let { value } = props
 
+    let mdText = marked(value)
+
     return (
         <div>
-            
+            <div dangerouslySetInnerHTML={{ __html: mdText }} />
         </div>
     )
 }
