ff4874d39926048135e4f14776eb66219ac53469
Souvik <souvikde.ns@gmail.com>
2020-10-31 06:23:40 +0530

    Update toolbar.jsx

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const options = {
+    HEADING: 'heading',
+    BOLD: 'bold',
+    ITALICS: 'italics',
+    QUOTE: 'quiote',
+    CODE: 'code',
+    LINK: 'link',
+    BLIST: 'blist',
+    NLIST: 'nlist',
+}
+
 const Toolbar = props => {
 
     return (
@@ -11,7 +22,7 @@ const Toolbar = props => {
 }
 
 Toolbar.propTypes = {
-    
+
 }
 
 export default Toolbar;
